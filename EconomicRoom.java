725a1f45032019555ffe3ef0b03adadb22b20218
Steven Tang <tangsteven90@gmail.com>
2018-12-02 14:50:12 -0800

    Update same as LuxRoom

--- EconomicRoom.java
+++ EconomicRoom.java
@@ -3,13 +3,12 @@ public class EconomicRoom extends CommonRoom{
 	static final double PRICE = 100.0;
 	private double price = PRICE;
 	
+	public EconomicRoom(int roomNum) {
+		super(roomNum);
+	}
+	
 	public double getPrice() {
 		return price;
 	}
-	
-	public EconomicRoom(int roomNum) {
-		roomNumber = roomNum;
-	}
-	
 	
 }
