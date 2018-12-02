2b458179a2b60b1b72967f375260a7a794e3005d
Steven Tang <tangsteven90@gmail.com>
2018-12-02 15:32:34 -0800

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -2,4 +2,12 @@ import java.util.*;
 
 public class Guest {
 	private ArrayList<Account> accounts;
+	
+	public Guest(){
+		accounts = new ArrayList<Account>();
+	}
+	
+	public addGuest(Account a){
+		accounts.add(a);
+	}
 }
