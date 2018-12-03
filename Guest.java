745ed0122f273d6bf69709700924c42d4fd130ef
Steven Tang <tangsteven90@gmail.com>
2018-12-02 21:34:54 -0800

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -10,4 +10,14 @@ public class Guest {
 	public addGuest(Account a){
 		accounts.add(a);
 	}
+	
+	public boolean verify(String username,String password) {
+		boolean accessible = false;
+		for(Account a : accounts) {
+			if(a.getID().equals(username) && a.getPassword().equals(password)) {
+				accessible = true;
+			}
+		}
+		return accessible;
+	}
 }
