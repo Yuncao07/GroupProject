fbc13cb9598e89db5664297c01cfff1e42f305b0
Steven Tang <tangsteven90@gmail.com>
2018-12-08 01:01:33 -0800

    MVC for booking rooms

--- BookingRoomPanel.java
+++ BookingRoomPanel.java
@@ -1,21 +1,39 @@
 import java.time.LocalDate;
 import javax.swing.*;
+import javax.swing.event.*;
+
 import java.util.*;
 
-public class BookingRoomPanel extends JPanel{ //Will need to do MVC on this --- not done yet
+public class BookingRoomPanel extends JPanel implements ChangeListener{ //Will need to do MVC on this --- not done yet
 	private JTextArea rooms;
 	private String text = "";
-	public BookingRoomPanel(LocalDate start,LocalDate end, RoomList list) {
+	private ArrayList<Room> listOfRooms;
+	private DateReservation stay;
+	private final RoomList l;
+	public BookingRoomPanel(RoomList list) {
 		rooms = new JTextArea();
-		DateReservation stay = new DateReservation(start,end);
-		text = text.concat(stay.toString() + "\n\n");
-		ArrayList<Room> availableRooms = list.getChosenAvailableRooms(stay);
-		for(Room r : availableRooms) {
+		rooms.setColumns(40);
+		stay = list.getDateReservation();
+		text = text.concat(stay.toString() + "\nRooms Available:\n");
+		l = list;
+		listOfRooms = l.getChosenAvailableRooms(stay);
+		for(Room r : listOfRooms) {
 			text = text.concat(r.getRoomNumber() + " ");
 		}
 		text.concat("\n");
 		rooms.setText(text);
 		rooms.setEditable(false);
 		add(rooms);
+	}
+	
+	public void stateChanged(ChangeEvent e) {
+		text = l.getDateReservation().toString() + "\nRooms Available:\n";
+		listOfRooms = l.getChosenAvailableRooms(l.getDateReservation());
+		for(Room r : listOfRooms) {
+			text = text.concat(r.getRoomNumber() + " ");
+		}
+		text.concat("\n");
+		rooms.setText(text);
+		repaint();
 	}
 }
