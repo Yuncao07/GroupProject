53362b5e176af02d33a14f17fb2ea8642970c758
keshuv <keshuv.bagaria@sjsu.edu>
2018-12-08 17:07:34 -0800

    javadoc

--- SignUpPanel.java
+++ SignUpPanel.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
-
+/**
+ * The class is a Panel that allows users to sign up with the system. 
+ */
 public class SignUpPanel extends JPanel{
 	private JLabel name,user,pass;
 	private JTextField nameField,userField,passwordField;
-	
+	/**
+	 * The constructor adds a couple of text fields for the users to enter their name, username and password to sign up.
+	 */
 	public SignUpPanel() {
 		setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
 		name = new JLabel("Name");
@@ -22,12 +26,23 @@ public class SignUpPanel extends JPanel{
 		add(passwordField);
 	}
 	
+	/**
+	 * @return the name of the user
+	 */
 	public String getName() {
 		return nameField.getText();
 	}
+	
+	/**
+	 * @return the username of the user
+	 */
 	public String getUser() {
 		return userField.getText();
 	}
+	
+	/**
+	 * @return the password of the user
+	 */
 	public String getPass() {
 		return passwordField.getText();
 	}
